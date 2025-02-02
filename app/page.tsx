702de4ba576e3565d9005c1b43ff3d6ac9710788
Mauro Risso <73178418+y0mauro@users.noreply.github.com>
2025-02-02 01:51:09 +0100

    Remove unused components and reset password page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 import Hero from "@/components/landing/hero";
 import Features from "@/components/landing/features";
-import GettingStarted from "@/components/landing/getting-started";
-import Footer from "@/components/shared/footer";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-16 my-16">
       <Hero />
-      {/* <GettingStarted /> */}
       <Features />
     </div>
   );
